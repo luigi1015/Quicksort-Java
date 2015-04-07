fb0f3511fc517aafc4bf62a365fbc4acd9dae346
luigi1015 <github@314ghz.com>
2015-04-07 02:16:27 -0500

    Added a unit test.

--- Quicksort/src/test/java/net/codehobby/quicksort/QuicksortTest.java
+++ Quicksort/src/test/java/net/codehobby/quicksort/QuicksortTest.java
@@ -49,6 +49,25 @@ public class QuicksortTest extends TestCase
     }
 
     /**
+     * Basic test of Quicksort's sorting using Integers. This time it uses the quicksort method with the left and right parameters.
+     */
+    public void testSortBasicIntegerLeftRight()
+    {
+		ArrayList<Integer> elementsToSort = new ArrayList<Integer>();
+		elementsToSort.add(3);
+		elementsToSort.add(1);
+		elementsToSort.add(4);
+		elementsToSort.add(1);
+
+		Quicksort.quicksort( elementsToSort, 0, 3 );
+
+		assertTrue( elementsToSort.get(0).equals(Integer.valueOf(1)) );
+		assertTrue( elementsToSort.get(1).equals(Integer.valueOf(1)) );
+		assertTrue( elementsToSort.get(2).equals(Integer.valueOf(3)) );
+		assertTrue( elementsToSort.get(3).equals(Integer.valueOf(4)) );
+    }
+
+    /**
      * Basic test of Quicksort's sorting using Strings.
      */
     public void testSortBasicString()
